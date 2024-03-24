143c286f347278e0b58aa82aff0adb6cfa9fc9bd
hdisra318 <hdisra318@ciencias.unam.mx>
2024-03-24 02:15:45 -0600

    Adding new project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,26 +1,34 @@
 import medfil from "../assets/projects/logo_medfil.jpeg";
 import calcApp from '../assets/projects/calc_app.png';
 import springProj from "../assets/projects/spring_proj.png";
+import youtubeClone from "../assets/projects/youtube_clone.png";
 
 function Projects() {
 
     const projects = [
         {
             id: 1,
+            src: youtubeClone,
+            title: "Proyecto \"Youtube Clone\"",
+            description: "Aplicación web similar a \"YouTube\" con algunas funciones similares. Desarrollada utilizando React, MUI y consumo de API's.",
+            link: "https://github.com/hdisra318/youtube-clone"
+        },
+        {
+            id: 2,
             src: springProj,
             title: "Proyecto \"Ensaladas Ciencias\"",
             description: "Aplicación web full stack realizada para un proyecto de la materia de \"Tecnologías para Desarrollos en Internet\" utilizando React, Bootstrap, Spring, SQL y Docker.",
             link: "https://github.com/hdisra318/Proyecto-React-Spring"
         },
         {
-            id: 2,
+            id: 3,
             src: medfil,
             title: "MedFil app",
             description: "Aplicación hecha para la Facultad de Medicina de la Universidad Nacional Autónoma de México (UNAM) que consiste en una app diseñada para que los estudiantes de medicina practiquen sus habilidades de diagnóstico a través de 'filtros' sobre algunas enfermedades/condiciones.",
             link: "https://github.com/hdisra318/MiztliMedFil"
         },
         {
-            id: 3,
+            id: 4,
             src: calcApp,
             title: "Calculdora",
             description: "App de una calculadora sencilla para Android, hecha en Android Studio.",
@@ -29,7 +37,7 @@ function Projects() {
     ]
 
     return (
-        <div name="Projects" className="bg-[#000a23] w-full text-white md:h-[110vh]">
+        <div name="Projects" className="bg-[#000a23] w-full text-white py-5">
             <div className="max-w-screen-lg p-4 pt-5 mx-auto flex flex-col justify-center items-center w-full h-full">
                 <div className="pb-8">
                     <h3 className="text-4xl font-bold inline border-b-4 border-blue-700">Projects</h3>
